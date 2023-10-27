ab2d879f95eeeaecb5452f5cca49a500aaf38e92
jackybarraza <jackybarraza@gmail.com>
2023-10-27 22:39:30 +0200

    building the component prices

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,15 +1,25 @@
 import { Logo } from "./Logo.jsx";
 import datos from "../datos.json";
+
+/**
+ * Header component for a minimalist website.
+ */
 export function Header() {
   return (
-    <div className="mt-4 d-flex justify-content-between">
-      <div className="d-flex">
-        <Logo></Logo>
-        <p className="fs-4">{datos.header.name}</p>
+    <div className="mt-3 mx-3 d-flex justify-content-between align-items-center">
+      {/* Logo and Website Name */}
+      <div className="d-flex align-items-center">
+        <Logo />
+        <p className="fs-5 ml-2 mb-0 ">{datos.header.name}</p>
       </div>
+
+      {/* Navigation Links */}
       <div>
         {datos.header.links.map((item, index) => (
-          <a key={index} className="mx-3 tex-decoration-none" href={item.url}>
+          <a
+            key={index}
+            className="mx-3 text-decoration-none text-dark"
+            href={item.link}>
             {item.text}
           </a>
         ))}
