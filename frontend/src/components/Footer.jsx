54ee7c6c18ec6fec08231306396e265d5dbefeaa
jackybarraza <jackybarraza@gmail.com>
2023-10-27 22:12:40 +0200

    building the component footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,3 +1,44 @@
+import datos from "../datos.json";
+
+/**
+ * Component to display a section of links.
+ * @param {object} data - The data object containing section information.
+ * @param {string} data.title - The title of the section.
+ * @param {object[]} data.links - An array of link objects with `title` and `url` properties.
+ */
+function Section({ data }) {
+  return (
+    <div>
+      <h5>{data.title}</h5>
+      <ul className="nav flex-column">
+        {data.links.map((item, index) => (
+          <li key={index}>
+            <a
+              className="text-decoration-none text-dark"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={item.url}>
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+/**
+ * Footer component for a minimalist website.
+ */
 export function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <div className="d-flex justify-content-between">
+      {/* Website Name */}
+      <div className="fs-4 mt-3">{datos.header.name}</div>
+      {/* Sections with Links */}
+      {datos.footer.map((item, index) => (
+        <Section key={index} data={item}></Section>
+      ))}
+    </div>
+  );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,3 +1,19 @@
+import { Logo } from "./Logo.jsx";
+import datos from "../datos.json";
 export function Header() {
-  return <h1>Header</h1>;
+  return (
+    <div className="mt-4 d-flex justify-content-between">
+      <div className="d-flex">
+        <Logo></Logo>
+        <p className="fs-4">{datos.header.name}</p>
+      </div>
+      <div>
+        {datos.header.links.map((item, index) => (
+          <a key={index} className="mx-3 tex-decoration-none" href={item.url}>
+            {item.text}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
 }
